226d48f4b72d650d7cacd970d326f35bdce08af8
MIGUEL DE PAULA VELOSO <miguel.pveloso@senacsp.edu.br>
2023-07-03 22:10:14 -0300

    up

--- index.php
+++ index.php
@@ -5,22 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="css/style.css">
+    <!--    links do bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <!--    css particular  -->
+    <link rel="stylesheet" href="css/style.css">
 
     <title>.: 05 :.</title>
 </head>
 <body>
+
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-danger">Submit</button>
+</form>
+
     <div id= "retangulo1" class= "retangulos"></div>
     <div class="retangulos"></div>
     <div class="retangulos"></div>
     <div class="retangulos"></div>
 
-    <br><br>
-
     <?php
         for ($i = 0; $i < 10; $i++)
         {
-            echo 'div class="retangulos"></div';
+            echo '<div class="retangulos"></div>';
         }
     ?>
 </body>
